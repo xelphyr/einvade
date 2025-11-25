e0deebbf4af4f9d9e32ddf51df19bf147d635aa7
xelphyr <168811362+xelphyr@users.noreply.github.com>
2025-11-25 14:27:14 +0530

    Update baseUrl in quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "worlds.xelphyr.xyz/einvade",
+    baseUrl: "einvade.worlds.xelphyr.xyz",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
